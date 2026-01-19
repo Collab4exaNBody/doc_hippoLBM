32bdfde2d80020aad9fd4f5a3ce3d84b220d5827
Raphaël Prat <60797514+rprat-pro@users.noreply.github.com>
2026-01-19 10:20:34 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ author = 'Raphaël Prat, Lhassan Amarsid'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx_rtd_theme', 'sphinxcontrib.bibtex', 'myst_parser','sphinx_tabs.tabs']
+extensions = ['sphinx_rtd_theme', 'sphinxcontrib.bibtex', 'myst_parser','sphinx_design']
 
 bibtex_bibfiles= ["bibliography.bib"]
 
